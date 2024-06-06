c6877e515d3a540f40a1cf8ee34376a728deb262
S0meDudeIdk <phamvuhoangbao2004@outlook.com>
2024-06-06 14:52:11 +0700

    Added Tile Prefabs + GameManager
    Adding GameManager for implemeting the core game later

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(SpriteRenderer))]
 
-public class Tile : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Tile : MonoBehaviour {
+    [Header("Tile Sprites")]
+    [SerializeField] private Sprite unclickedTile;
+    [SerializeField] private Sprite flaggedTile;
+    [SerializeField] private List<Sprite> clickedTile;
+    [SerializeField] private Sprite mineTile;
+    [SerializeField] private Sprite mineWrongTile;
+    [SerializeField] private Sprite mineHitTile;
+
+    private SpriteRenderer spriteRenderer;
+    public bool flagged = false;
+    public bool active = true;
+    public bool isMine = false;
+    public int mineCount = 0;
+
+    void Awake() {
+        // Must exists
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnMouseOver() {
+        // If it hasn't already been pressed
+        if (active) {
+            if (Input.GetMouseButtonDown(0)) {
+                // Left click = Reveal tile contents
+                ClickedTile();
+            } else if (Input.GetMouseButtonDown(1)) {
+                // Right click = Toggle flag
+                flagged = !flagged;
+                if (flagged) { 
+                    spriteRenderer.sprite = flaggedTile;
+                } else {
+                    spriteRenderer.sprite = unclickedTile;
+                }
+            }
+        }
+    }
+
+    public void ClickedTile() {
+        // Flagged => Not allow left click
+        if (active & !flagged) {
+            // Makes sure that left click cannot click again
+            active = false;
+            if (isMine) {
+                // "You are ded. Not big surprise :3" - Heavy from TF2
+                spriteRenderer.sprite = mineHitTile;
+            } else {
+                // Safe click, set the correct sprite
+                spriteRenderer.sprite = clickedTile[mineCount];
+            }
+        }
     }
 }
