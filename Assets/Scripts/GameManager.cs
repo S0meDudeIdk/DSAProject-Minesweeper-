d4e271b065670c51a497926f0174a217e0d46d9c
S0meDudeIdk <phamvuhoangbao2004@outlook.com>
2024-06-07 09:30:35 +0700

    Creating the Board in GameManager
    The Board is now set at the center with Tiles.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
+    [SerializeField] private Transform tilePrefab;
+    [SerializeField] private Transform gameHolder;
+
+    private int width;
+    private int height;
+    private int numMines;
+
+    private readonly float tileSize = 0.5f;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        CreateGameBoard(9, 9, 10);                  // Beginner
+        // CreateGameBoard (16, 16, 40);            // Intermidiate
+        // CreateGameBoard (30, 16, 99);            // Expert
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void CreateGameBoard(int width, int height, int numMines) {
+        // Save the game parameters
+        this.width = width;
+        this.height = height;
+        this.numMines = numMines;
+
+        // Create a Board with tiles
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                // Position the tile in the center
+                Transform tileTransform = Instantiate(tilePrefab);
+                tileTransform.parent = gameHolder;
+
+                float xIndex = col - ((width - 1) / 2.0f);
+                float yIndex = row - ((height - 1) / 2.0f);
+                tileTransform.localPosition = new Vector2(xIndex * tileSize,
+                                                            yIndex * tileSize);
+            }
+        }
     }
 }
